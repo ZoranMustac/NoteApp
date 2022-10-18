e0a3322a25682966fd7ef63b4ecd8662fa5f9546
zm2666 <83871614+zm2666@users.noreply.github.com>
2022-10-18 22:44:57 +0200

    Created UI layout

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,17 @@
 import React from "react";
+import '../css/styles.css';
 
 function Home(){
     return(
         <div>
             <h1 className="text-5xl font-medium text-center">Notes App</h1>
+            <div className="pt-12">
+            <div className="container overflow-hidden px-8 rounded-xl aligning">
+                <div className="grid grid-cols-4 gap-4 box-content justify-items-center">
+                    <button type="button" className="bg-red-700 text-white p-12 text-lg rounded-2xl">Add note</button>
+                </div>
+            </div>
+            </div>
         </div>
     )
 }
